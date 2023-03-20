617643068c5754315e33471e552cd88a234bfb63
Nada <kokohend81@gmail.com>
2023-03-20 23:36:57 +0200

    first commit

---
+++ Assignment1.dart
@@ -0,0 +1,12 @@
+// Exercise 2
+// Ask the user for a number. Depending on whether the number is even or odd,
+// print out an appropriate message to the user.
+
+void main() {
+  int num = 10;
+  if (num % 2 == 0) {
+    print('${num} is even');
+  } else {
+    print('${num} is odd');
+  }
+}
